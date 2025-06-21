568f99fbadc95634c28d7c4f1f8c72f115e4587b
Wilmar73 <wilmar310799@gmail.com>
2025-06-21 19:37:32 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿//1.Declarar variable de los diferentes tipos, asignarles valor e imprimir el valor. /
+decimal precio = 19.99m;    
+int cantidad = 5;
+float descuento = 0.15f;    
+string producto = "Camiseta";
+bool estaDisponible = true;
+
+//inprimir los valores de las variables
+Console.WriteLine("Valor del precio" + precio);
+Console.WriteLine("Valor de la cantidad: " + cantidad);
+Console.WriteLine("Valor del descuento: " + descuento);
+Console.WriteLine("Valor del producto: " + producto);
+Console.WriteLine("¿Está disponible? " + estaDisponible);
+
+//2.Buscar cómo se declara una constante en C# e imprimir el valor. Probar de cambiar su valor luego y ver que es lo que pasa.
+  // const double PI = 3.14159; // Declaración de una constante  
+//Console.WriteLine("Valor de PI: " + PI);/
+// Imprimir el valor de la constante PI
+// Intentar cambiar el valor de la constante (esto generará un error de compilación)
+// PI = 3.14; // Descomentar esta línea generará un error de compilación porque PI es una constante
+// Console.WriteLine("Nuevo valor de PI: " + PI); // Esto no se ejecutará porque la línea anterior generará un error
+
+
+
+
+
+
+
+
+
+
